833ab2a0dc10d94a8d23e727057f613dfa2fd538
Bhavik Makwana <bhavikmakwana1993@gmail.com>
2019-03-24 16:15:30 +0530

    ngModel added

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-
+  about: AboutModel = {
+    email : '',
+    password : '',
+    address: '',
+    address2: '',
+    city: '',
+    state: '',
+    zip: '',
+  };
   constructor() { }
 
   ngOnInit() {
   }
 
+  submitForm(): void {
+    console.log(this.about.email);
+  }
+
+}
+
+export interface AboutModel {
+  email: string;
+  password: string;
+  address: string;
+  address2: string;
+  city: string;
+  state: string;
+  zip: string;
 }
